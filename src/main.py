85dc1fd0580de49032875bcf81bcbd6560909f4f
Jose R. Krauss <jose.ricardo.k@gmail.com>
2021-04-22 16:59:52 -0300

    minor code refactoring

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import click
 import os
 import csv
 from questionary import questionary, Choice
-import xml.etree.ElementTree as ET
-#from pymongo import MongoClient, InsertOne
 from .game import GameCampeonatoBrasileiro, GameCopaDoBrasil
 from bs4 import BeautifulSoup
 from concurrent.futures import as_completed
@@ -14,11 +12,15 @@ from requests_futures.sessions import FuturesSession
 def scrapper(gameq, competition, division, season, is_all_games, file_format):    
     start_time = time.time()
 
-    print(f'Fetching game data for competition {competition}...')
+    print(f'\nFetching game data for competition: {competition} {season}...')
 
     if competition == 'campeonato-brasileiro':
-        with FuturesSession(max_workers=10) as session:
-            futures = [session.get(f'https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-{division}/{season}/{game}') for game in range(1, 381 if is_all_games else 11)]
+        
+        qty = 381 if is_all_games else 6 # How many games will be fetched
+        wrks = 10 if is_all_games else 5 # number of threads 
+
+        with FuturesSession(max_workers=wrks) as session:
+            futures = [session.get(f'https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-{division}/{season}/{game}') for game in range(1, qty)]
 
             for req in as_completed(futures):
                 if req.result().status_code == 200:
@@ -27,9 +29,13 @@ def scrapper(gameq, competition, division, season, is_all_games, file_format):
                     gameq.append(bfgame)
 
     else:
-        with FuturesSession(max_workers=10) as session:
+
+        qty = 173 if is_all_games else 6 # How many games will be fetched
+        wrks = 10 if is_all_games else 5 # number of threads
+        
+        with FuturesSession(max_workers=wrks) as session:
             
-            futures = [session.get(f'https://www.cbf.com.br/futebol-brasileiro/competicoes/copa-brasil-masculino/{season}/{game}') for game in range(1, 173 if is_all_games else 11)]
+            futures = [session.get(f'https://www.cbf.com.br/futebol-brasileiro/competicoes/copa-brasil-masculino/{season}/{game}') for game in range(1, qty)]
 
             for req in as_completed(futures):
                 if req.result().status_code == 200:
@@ -38,40 +44,35 @@ def scrapper(gameq, competition, division, season, is_all_games, file_format):
                     gameq.append(bfgame)
 
     stop_time = time.time()
-    print(f'Finished in {stop_time-start_time}')
+    print(f'Fetching finished in {stop_time-start_time:.3} sec')
 
 #---------------------------------- Game processing function
 def dumper(gameq, competition, division, season, is_all_games, file_format):
     start_time = time.time()
-    #client = MongoClient(host='localhost', port=27017)
-
-    #for game in sorted(game_queue, key=lambda x: x.competition_header['game_number']):
-    #if client.football.brasileiro_serie_B.insert_one(game.get_game_dict()):
-            #print(f"Game {game.competition_header['game_number']} saved successfully!")
 
     if file_format == 'json':        
         for game in gameq:
             try:
-                with open(os.path.join(os.getcwd(), 'games', f'{game}.json'), 'w', encoding='utf-8') as json_file:
+                with open(os.path.join(os.getcwd(), 'export', f'{game}.json'), 'w', encoding='utf-8') as json_file:
                     json_file.write(game.get_game_json())
             except OSError as err:
                 print(f'Error: permission error {os.strerror(err.errno)}, stack_trace: {err.with_traceback()}')
 
-            print(f"File ./games/{game}.json saved!")
+            print(f"File ./export/{game}.json saved!")
     
     elif file_format == 'xml':
         for game in gameq:
             try:
-                game.get_game_xml().write(os.path.join(os.getcwd(), 'games', f'{game}.xml'), encoding='utf-8', method='xml', xml_declaration=True)
+                game.get_game_xml().write(os.path.join(os.getcwd(), 'export', f'{game}.xml'), encoding='utf-8', method='xml', xml_declaration=True)
             except OSError as err:
                 print(f'Error: permission error {os.strerror(err.errno)}, stack_trace: {err.with_traceback()}')
                 
-            print(f"File ./games/{game}.xml saved!")
+            print(f"File ./export/{game}.xml saved!")
     
     else:            
-        with open(os.path.join(os.getcwd(), 'games', f'{competition}-{division}-{season}.csv'), 'w', encoding='utf-8', newline='') as csv_file:
+        with open(os.path.join(os.getcwd(), 'export', f'{competition}-{division}-{season}.csv'), 'w', encoding='utf-8', newline='') as csv_file:
             try:
-                writer = csv.writer(csv_file, delimiter=';')                
+                writer = csv.writer(csv_file, delimiter=';')                              
                 for index, game in enumerate(gameq):
                     if index == 0:
                         writer.writerow(game.get_game_csv()[0])
@@ -79,16 +80,16 @@ def dumper(gameq, competition, division, season, is_all_games, file_format):
             except PermissionError as err:
                 print(f'Error: permission error {os.strerror(err.errno)}, stack_trace: {err.with_traceback()}')
         
-        print(f"File ./games/{competition}-{division}-{season}.csv saved!")
+        print(f"File ./export/{competition}-{division}-{season}.csv saved!")
 
     stop_time = time.time()
-    print(f'Finished in {stop_time-start_time}')
+    print(f'Saving finished in {stop_time-start_time:.3} sec')
 
 #---------------------------------- 
 bfgame_queue = []
 
 #---------------------------------- Options available
-lst_competition = [Choice('Campeonato Brasileiro', value='campeonato-brasileiro', checked=True), 
+lst_competition = [Choice('Campeonato Brasileiro', value='campeonato-brasileiro'), 
                     Choice('Copa do Brasil', value='copa-do-brasil')]
 lst_divison = [Choice('Serie A', value='a'), Choice('Serie B', value='b')]
 lst_season = [str(year) for year in range(2012, 2021)]
@@ -96,10 +97,10 @@ lst_file_format = [Choice('json'), Choice('xml'), Choice('csv')]
 
 #---------------------------------- Main method
 @click.command()
-@click.option('-c', '--competition', type=click.Choice(['campeonato-brasileiro', 'copa-do-brasil'], case_sensitive=False), help="What competition ('campeonato-brasileiro', 'copa-do-brasil')")
-@click.option('-d', '--division', type=click.Choice(['a', 'b'], case_sensitive=False), help="The competition's division (A or B)")
+@click.option('-c', '--competition', type=click.Choice(['campeonato-brasileiro', 'copa-do-brasil'], case_sensitive=False))
+@click.option('-d', '--division', type=click.Choice(['a', 'b'], case_sensitive=False))
 @click.option('-s', '--season', type=click.IntRange(2012, 2020, clamp=True), help='What season, starting from 2012...2020')
-@click.option('-f', '--file-format', type=click.Choice(['json', 'xml', 'csv'], case_sensitive=False), help='What format the game match files will be saved in (xml, json or csv)')
+@click.option('-f', '--file-format', type=click.Choice(['json', 'xml', 'csv'], case_sensitive=False))
 @click.option('--sample', is_flag=True, help="Extracts only the first 10 game matches")
 def main(competition=None, division=None, season=None, file_format=None, sample=False):
     """Brazillian Football Web Scrapper - CLI"""
@@ -113,7 +114,7 @@ def main(competition=None, division=None, season=None, file_format=None, sample=
         competition = questionary.select("Select a competition", choices=lst_competition).ask()
         division = questionary.select("Select a division", choices=lst_divison).skip_if(competition == 'copa-do-brasil', default='a').ask()
         season = questionary.select("Select a season", choices=lst_season).ask()
-        is_all_games = questionary.confirm("Do you want to extract all the games").ask()
+        is_all_games = questionary.confirm("Download all the games?").ask()
         file_format = questionary.select("Select a file format", choices=lst_file_format).ask()
 
         scrapper(bfgame_queue, competition, division, season, is_all_games, file_format)
